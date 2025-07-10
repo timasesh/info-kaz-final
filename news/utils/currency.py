19c439bdea337d99dd7470bde2d6c1f101dc8e66
Aibek <aibek.aibek.1204@gmail.com>
2025-07-10 19:47:50 +0500

    USD EURO RUB 2

--- news/utils/currency.py
+++ news/utils/currency.py
@@ -14,4 +14,34 @@ def get_usd_kzt_rate():
                 return float(rate.replace(',', '.'))
         return None
     except Exception:
-        return None 
+        return None
+
+def get_eur_kzt_rate():
+    url = "https://nationalbank.kz/rss/rates_all.xml"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        root = ET.fromstring(response.content)
+        for item in root.findall('.//item'):
+            title = item.find('title').text
+            if title == 'EUR':
+                rate = item.find('description').text
+                return float(rate.replace(',', '.'))
+        return None
+    except Exception:
+        return None
+
+def get_rub_kzt_rate():
+    url = "https://nationalbank.kz/rss/rates_all.xml"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        root = ET.fromstring(response.content)
+        for item in root.findall('.//item'):
+            title = item.find('title').text
+            if title == 'RUB':
+                rate = item.find('description').text
+                return float(rate.replace(',', '.'))
+        return None
+    except Exception:
+        return None
